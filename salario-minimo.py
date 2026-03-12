f5ac43df45200cd6149b07b8870cc37a8b8122ca
Igor Francisco Antunes Paulo <115815356+DevIgorPaulo@users.noreply.github.com>
2026-03-12 20:36:33 -0300

    Add files via upload

---
+++ salario-minimo.py
@@ -0,0 +1,4 @@
+salarioMinimo = float(input("Informe o sálario minimo atual: "))
+salarioMensal = float(input("Informe seu sálario mensal: "))
+qtdSalariosMinimos = salarioMensal / salarioMinimo
+print(f"Você recebe {qtdSalariosMinimos} salario(s) minimo(s)")
