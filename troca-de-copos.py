fcbdbbc334f2a6c2f27bb30882c26e0403afefbb
Igor Francisco Antunes Paulo <115815356+DevIgorPaulo@users.noreply.github.com>
2026-03-18 19:35:39 -0300

    Troca de variaveis

---
+++ troca-de-copos.py
@@ -0,0 +1,15 @@
+copo1 = "Laranja"
+copo2 = "Acerola"
+
+print(f"Copo 1 inicio: {copo1}")
+print(f"Copo 2 inicio: {copo2}")
+
+copo3 = copo1
+copo4 = copo2
+
+copo1 = copo4
+copo2 = copo3
+
+print(f"Copo 1 fim: {copo1}")
+print(f"Copo 2 fim: {copo2}")
+
